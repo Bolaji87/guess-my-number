867d14cfb25d05f0e0824750f6af575af5f6d8dc
bolaji87 <sakabolaji77@gmail.com>
2024-08-27 22:40:33 +0100

    feat:add chnages to Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 export default function Header() {
+  const randomNum = Math.floor(Math.random() * 6 + 1);
+
   return (
     <header>
       <h1>Guess My Number!</h1>
       <p className="between">(Between 1 and 20)</p>
       <button className="btn again">Again!</button>
-      <div className="number">?</div>
+      <div className="number">{randomNum}</div>
     </header>
   );
 }
