e2e515739bc5be1aefb321af4713e5d89e23c8dc
bolaji87 <sakabolaji77@gmail.com>
2024-10-09 14:59:20 +0100

    feat: pass secretNumber prop and use it Header.jsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,14 +1,10 @@
-import React from "react";
-
-export default function Header() {
-  const randomNum = Math.floor(Math.random() * 6 + 1);
-
+export default function Header({ secretNumber }) {
   return (
     <header>
       <h1>Guess My Number!</h1>
       <p className="between">(Between 1 and 20)</p>
       <button className="btn again">Again!</button>
-      <div className="number">{randomNum}</div>
+      <div className="number">{secretNumber}</div>
     </header>
   );
 }
