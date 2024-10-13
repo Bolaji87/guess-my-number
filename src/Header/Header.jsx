e2ed29a0b8d67020858d681834c92bf915238556
bolaji87 <sakabolaji77@gmail.com>
2024-10-13 15:25:39 +0100

    feat: add secretNumber,width state and reset handler

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,14 @@
-export default function Header({ secretNumber }) {
+export default function Header({ secretNumber, width, onReset }) {
   return (
     <header>
       <h1>Guess My Number!</h1>
       <p className="between">(Between 1 and 20)</p>
-      <button className="btn again">Again!</button>
-      <div className="number">{secretNumber}</div>
+      <button onClick={onReset} className="btn again">
+        Again!
+      </button>
+      <div style={{ width: width }} className="number">
+        {secretNumber}
+      </div>
     </header>
   );
 }
