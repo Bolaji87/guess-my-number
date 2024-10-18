81ca340e6ccbdcbbebccaaf866948f2591d043d4
bolaji87 <sakabolaji77@gmail.com>
2024-10-18 23:10:50 +0100

    feat: moved all the states inside context

---
+++ src/hooks/GuessContext.jsx
@@ -0,0 +1,110 @@
+import React, { createContext, useReducer } from "react";
+
+const GuessContext = createContext();
+const secretNumber = Math.floor(Math.random() * 20 + 1);
+
+function GuessProvider({ children }) {
+  const initialState = {
+    guess: "",
+    message: "⛔ Start guessing...",
+    score: 20,
+    highscore: 0,
+    background: null,
+    width: "15rem",
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "guessControlled":
+        return { ...state, guess: action.payload };
+      case "correctNumber":
+        return {
+          ...state,
+          message: action.payload,
+          highscore:
+            state.score > state.highscore ? state.score : state.highscore,
+          width: "30rem",
+          background: "#60b347",
+        };
+
+      case "tooHigh":
+        return { ...state, message: action.payload };
+      case "tooLow":
+        return { ...state, message: action.payload };
+      case "wrongGuess":
+        return { ...state, score: action.payload };
+      case "gameover":
+        return { ...state, message: action.payload, score: 0 };
+      case "reset":
+        return {
+          ...state,
+          score: 20,
+          message: "⛔ Start guessing...",
+          highscore: state.highscore,
+          guess: action.payload,
+          width: "15rem",
+          background: "#222",
+        };
+
+      default:
+        throw new Error("unknown action type");
+    }
+  };
+
+  const [{ guess, message, score, highscore, width, background }, dispatch] =
+    useReducer(reducer, initialState);
+
+  function handleChangeGuess(e) {
+    dispatch({
+      type: "guessControlled",
+      payload: Number(e.target.value),
+    });
+  }
+
+  function handleSubmitGuess(e) {
+    e.preventDefault();
+    if (!guess) return;
+
+    if (guess === secretNumber) {
+      dispatch({ type: "correctNumber", payload: "🎉 Correct Number!" });
+    } else if (guess > secretNumber) {
+      if (score > 1) {
+        dispatch({ type: "wrongGuess", payload: score - 1 });
+        dispatch({ type: "tooHigh", payload: "📈 Too high!" });
+      } else {
+        dispatch({ type: "gameover", payload: "💥 You just lost the game" });
+      }
+    } else if (guess < secretNumber) {
+      if (score > 1) {
+        dispatch({ type: "wrongGuess", payload: score - 1 });
+        dispatch({ type: "tooLow", payload: "📉 Too low!" });
+      } else {
+        dispatch({ type: "gameover", payload: "💥 You just lost the game" });
+      }
+    }
+  }
+
+  function handleReset(e) {
+    dispatch({ type: "reset", payload: Number(e.target.value) });
+  }
+  return (
+    <GuessContext.Provider
+      value={{
+        guess,
+        score,
+        message,
+        highscore,
+        width,
+        background,
+        secretNumber,
+        onChangeGuess: handleChangeGuess,
+        onsubmitGuess: handleSubmitGuess,
+        onResetGuess: handleReset,
+      }}
+    >
+      {children}
+    </GuessContext.Provider>
+  );
+}
+
+export { GuessProvider, GuessContext };
